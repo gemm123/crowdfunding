f253621fcc249191bfd80b288c9def7f72e6032c
gemm123 <gemaakbar213@gmail.com>
2022-07-18 22:58:13 +0700

    init commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gemm123/crowdfunding/handler"
+	"github.com/gemm123/crowdfunding/user"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userRepositoy := user.NewRepository(db)
+	userService := user.NewService(userRepositoy)
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+}
